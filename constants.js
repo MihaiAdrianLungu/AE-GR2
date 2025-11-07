1e5c2064c780a177e76e96b8993346dc85230f96
Mihai Adrian Lungu <lungu.snk199@gmail.com>
2025-11-07 19:39:37 +0200

    exercised variables

---
+++ constants.js
@@ -0,0 +1,45 @@
+// var var1 = 1;
+// let var2 = 2;
+// const var3 = 3;
+
+// var1 = 5;
+// console.log(var1);
+
+// var2 = 5;
+// console.log(var2);
+
+// var3 = 5;
+// console.log(var3);
+
+// testVariables(true);
+
+function testVariables(param) {
+  console.log(var1);
+  if (param === true) {
+    var var1 = 1;
+    let var2 = 2;
+    const var3 = 3;
+  }
+
+  console.log(var1);
+  console.log(var2);
+  console.log(var3);
+}
+
+
+const testVariables2 = (param) => {
+    console.log(var1);
+    if (param === true) {
+        var var1 = 1;
+        let var2 = 2;
+        const var3 = 3;
+    }
+    
+  console.log(var1);
+  console.log(var2);
+  console.log(var3);
+};
+
+testVariables2(true);
+
+// testVariables(true);
